30a97448297df96a1470c5837428161fd699b6ed
haccerra <haccerra@aol.com>
2026-04-13 18:26:30 +0200

    Method AllocateUknownSessionName

--- internal/storage/repo.go
+++ internal/storage/repo.go
@@ -41,4 +41,27 @@ func (r *Repository) DB() *sql.DB {
 	return r.db
 }
 
+func (r *Repository) AllocateUknownSessionName(ctx context.Context) (string, int, error) {
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return "", 0, err
+	}
+	defer tx.Rollback()
+
+	counter, err := r.getStateIntTx(ctx, tx, "unknown_counter")
+	if err != nil {
+		return "", 0, err
+	}
+	counter++
+	if err := r.setStateTx(ctx, tx, "unknown_counter", strconv.Itoa(counter)); err != nil {
+		return "", 0, err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return "", 0, err
+	}
+
+	return fmt.Sprintf("unknown session (%d)", counter), counter, nil
+}
+
 
